801d8094b406523b4a282415ba2790443ff858d7
Julian Camilo <47187585+iRetray@users.noreply.github.com>
2020-07-02 15:50:58 -0500

    Creados datos locales JSON

--- todo/src/components/Tasks.js
+++ todo/src/components/Tasks.js
@@ -1,19 +1,8 @@
 import React from 'react'
 import NavBar from './NavBar'
 import Task from './Task'
-import * as firebase from 'firebase'
-
-const firebaseConfig = {
-    apiKey: "AIzaSyAGZ0ow2ZidCd2mE0CXIuNPu3P_3Vyxwn8",
-    authDomain: "todoreact-cbb4b.firebaseapp.com",
-    databaseURL: "https://todoreact-cbb4b.firebaseio.com",
-    projectId: "todoreact-cbb4b",
-    storageBucket: "todoreact-cbb4b.appspot.com",
-    messagingSenderId: "129372449365",
-    appId: "1:129372449365:web:f02cc207506dea506b82dc"
-  };
-firebase.initializeApp(firebaseConfig)
-
+import data from '../data.json'
+console.log(data);
 
 
 export default class Tasks extends React.Component {
@@ -21,24 +10,10 @@ export default class Tasks extends React.Component {
     constructor() {
         super()
         this.state = {
-            tittle: '',
-            description: ''
+            tittle: 'Tittle',
+            description: 'Description'
         }
-        const tittle = firebase.database().ref().child('tareas').child('0').child('tittle');
-        tittle.on('value', snapshot => {
-            this.setState({
-                tittle: snapshot.val()
-            })
-        })
-        const description = firebase.database().ref().child('tareas').child('0').child('description');
-        description.on('value', snapshot => {
-            this.setState({
-                description: snapshot.val()
-            })
-        })
-    }
-
-    
+    }    
 
     render() {
         return(
