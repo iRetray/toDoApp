22196a6180bf0ac471c92ff739317e0d24ba4b62
Julian Camilo <47187585+iRetray@users.noreply.github.com>
2020-07-05 15:42:30 -0500

    Correciones para 404

--- todo/src/components/NotFound.js
+++ todo/src/components/NotFound.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import e404 from '../img/404.jpg'
+import NavBarCollapse from './NavBarCollapse'
 
 export default class NotFound extends React.Component {
     render() {
         return(
             <div>
-                Imagen de NotFound
+                <NavBarCollapse />
+                <div className="container">
+                    <img src={e404} alt="" className="img-fluid mx-auto d-block "/>
+                </div>                
             </div>
         )
     }
