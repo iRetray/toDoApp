090bd26215676c8ca2288db3c42f2d4fb0e636a4
Julian Camilo <47187585+iRetray@users.noreply.github.com>
2020-07-05 15:27:39 -0500

    Diseño de devInfo

--- todo/src/components/DevInfo.js
+++ todo/src/components/DevInfo.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import NavBarCollapse from './NavBarCollapse'
 import nick from '../img/nickStyle.png'
+import { Button } from 'reactstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithubSquare, faFacebookSquare, faInstagramSquare }  from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faFacebookSquare, faInstagram }  from '@fortawesome/free-brands-svg-icons'
 
 export default class DevInfo extends React.Component {
     render() {
@@ -18,11 +19,15 @@ export default class DevInfo extends React.Component {
                             <p className="display-4">Retray</p>                            
                             <p className="lead">Desarrollador Web</p>
                             <hr></hr>
-                            <p className="lead text-muted">
-                                <FontAwesomeIcon icon={faGithubSquare}></FontAwesomeIcon> https://github.com/iRetray <br />
-                                <FontAwesomeIcon icon={faFacebookSquare}></FontAwesomeIcon> https://www.facebook.com/JRetray/ <br />
-                                <FontAwesomeIcon icon={faInstagramSquare}></FontAwesomeIcon> https://www.instagram.com/le_retray/
-                            </p>
+                            <Button color="link" href="https://github.com/iRetray">
+                                <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon> GitHub
+                            </Button>
+                            <Button color="link" href="https://www.facebook.com/JRetray/">
+                                <FontAwesomeIcon icon={faFacebookSquare}></FontAwesomeIcon> Facebook
+                            </Button>
+                            <Button color="link" href="https://www.instagram.com/le_retray/">
+                                <FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon> Instagram
+                            </Button>
                         </div>
                     </div>
                     <hr/>
