205116ad4cebcfb587e20c9be2d1348db7a606d0
Julian Camilo <47187585+iRetray@users.noreply.github.com>
2020-07-05 11:03:08 -0500

    Diseño del devZone

--- todo/src/components/DevInfo.js
+++ todo/src/components/DevInfo.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import NavBar from './NavBar'
+import nick from '../img/nickStyle.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithubSquare, faFacebookSquare, faInstagramSquare }  from '@fortawesome/free-brands-svg-icons'
 
 export default class DevInfo extends React.Component {
     render() {
         return(
             <div>
                 <NavBar />
-                <h1>Informacion del developer</h1>
-                Iconos diseñados por <a href="https://www.flaticon.es/autores/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.es/" title="Flaticon"> www.flaticon.es</a>
-                <a href='https://www.freepik.es/fotos-vectores-gratis/negocios'>Vector de Negocios creado por katemangostar - www.freepik.es</a>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-3">
+                            <img src={nick} alt="" className="img-fluid"/>
+                        </div>
+                        <div className="col-9">
+                            <p className="display-4">Retray</p>                            
+                            <p className="lead">Desarrollador Web</p>
+                            <hr></hr>
+                            <p className="lead text-muted"><FontAwesomeIcon icon={faGithubSquare}></FontAwesomeIcon> https://github.com/iRetray</p>
+                            <p className="lead text-muted"><FontAwesomeIcon icon={faFacebookSquare}></FontAwesomeIcon> https://www.facebook.com/JRetray/</p>
+                            <p className="lead text-muted"><FontAwesomeIcon icon={faInstagramSquare}></FontAwesomeIcon> https://www.instagram.com/le_retray/</p>
+                        </div>
+                    </div>
+                    <hr/>
+                </div>
             </div>
         )
     }
